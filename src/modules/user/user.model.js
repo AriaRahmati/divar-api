1833503c1b151c4261041aeb722f3a73e4a4cc2b
Aria Rahmati <ariaarahmati@gmail.com>
2024-04-09 13:58:26 +0330

    add user model

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const OTPSchema = new Schema({
+  code: { type: String, default: undefined },
+  expiresIn: { type: Number, default: 0 },
+});
+
+const userSchema = new Schema(
+  {
+    fullName: { type: String },
+    mobile: { type: String, unique: true, required: true },
+    otp: { type: OTPSchema },
+    verifiedMobile: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = model('User', userSchema);
+
+module.exports = {
+  userModel,
+};
